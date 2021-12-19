bb67c258be702cf3abcf9b40fb5ee422c31d68b1
Ulises Vargas <64346738+UliVargas@users.noreply.github.com>
2021-12-19 04:11:34 -0600

    Delete src/controller directory

--- src/controller/estacionamientos.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-
-const getEstacionamientos = async(req, res) => {
-    const resp = await axios.get("https://dev.parcoapp.com/api/Parkings").then(data => data.data);
-
-    res.status(200).json(resp)
-};
-
-module.exports = getEstacionamientos;
